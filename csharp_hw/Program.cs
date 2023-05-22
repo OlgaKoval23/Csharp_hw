6b9e61381a8ecb826074573bb78689cfb6d1073d
Olga_Koval <lispodliz@mail.ru>
2023-05-22 20:30:42 +1000

    Добавлено ДЗ № 4 со скринами

--- csharp_hw/Program.cs
+++ csharp_hw/Program.cs
@@ -61,3 +61,81 @@
 // {
 //     Console.WriteLine(nv3 +" - " +Math.Pow(nv3,3));
 // }
+
+
+
+// ДЗ к семинару № 4
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// int Stepen(int a, int b)
+// {
+// int res = 1;
+// for (int i = 1; i <= b; i++)
+// {
+//     res = res*a;
+// }
+// return res;
+// }
+// Console.Write("Введите число a = ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число b = ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int result = Stepen(a,b);
+// Console.WriteLine("Число a в степени b = " +result);
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// int Summa(int n)
+// {
+//     int s = 0;
+//     while (n > 0)
+//     {
+//         s = s + n%10;
+//         n = n/10;
+//     }
+//     return s;
+// }
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = Summa(num);
+// Console.WriteLine("Сумма цифр этого числа: "+result);
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[] RandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+
+// void WriteArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.Write("Введите длину массива: ");
+// int length = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите минимальный элемент массива: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите максимальный элемент массива: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// int[] myArray = RandomArray(length, min, max);
+// WriteArray(myArray);
